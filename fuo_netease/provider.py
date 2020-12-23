bada9d1780fa6407c0dc0800f61103357d434d61
cosven <yinshaowen241@gmail.com>
2020-12-24 00:08:36 +0800

    WIP: try to use model v2

--- fuo_netease/provider.py
+++ fuo_netease/provider.py
@@ -1,6 +1,7 @@
 import logging
 
-from feeluown.library import AbstractProvider, ProviderV2, ProviderFlags
+from feeluown.library import AbstractProvider, ProviderV2, ProviderFlags as PF
+from feeluown.media import Quality
 from feeluown.models import ModelType
 from .api import API
 
@@ -13,7 +14,7 @@ class NeteaseProvider(AbstractProvider, ProviderV2):
         identifier = 'netease'
         name = '网易云音乐'
         flags = {
-            ModelType.song: ProviderFlags.similar,
+            ModelType.song: PF.model_v2 | PF.similar | PF.multi_quality,
         }
 
     def __init__(self):
@@ -36,6 +37,25 @@ class NeteaseProvider(AbstractProvider, ProviderV2):
     def song_list_similar(self, song):
         songs = self.api.get_similar_song(song.identifier)
         return [_deserialize(song, NeteaseSongSchema) for song in songs]
+
+    def song_list_quality(self, song):
+        songs = self.api.weapi_songs_url([int(song.identifier)], 999000)
+        mapping = {}
+        if songs and songs[0]['url']:
+            # TODO: parse songs list and get more reasonable mapping
+            mapping = {
+                Quality.Audio.sq: songs[0]['url']
+            }
+        ttl = 60 * 20
+        song.cache_set('quality_media_mapping', mapping, ttl)
+        return list(mapping.keys())
+
+    def song_get_media(self, song, quality):
+        mapping = song.cache_get('quality_media_mapping')
+        if mapping is not None:
+            quality_list = self.song_list_quality(song)
+            if quality in quality_list:
+                return song.cache_get('quality_media_mapping')[quality]
 
 
 provider = NeteaseProvider()
