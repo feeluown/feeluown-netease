015317d33871f7e43161682184b8e7a9326d92b0
cyliuu <lcy940903@gmail.com>
2022-03-13 22:56:42 +0800

    use s_id and id to identify whether a cloud song has been rematched to offical database

--- fuo_netease/models.py
+++ fuo_netease/models.py
@@ -61,24 +61,30 @@ def create_cloud_songs_g(func, func_extra, schema=None, schema_extra=None,
         for idx, song_data in enumerate(songs_data[data_field]):
             if data_key:
                 song_data = song_data[data_key]
-            try:
-                song = _deserialize(song_data, schema)
-            except ValidationError:
-                # FIXME: 有些云盘歌曲在 netease 上不存在，这时不能把它们转换成
-                # SongModel。因为 SongModel 的逻辑没有考虑 song 不存在的情况。
-                # 这类歌曲往往没有 ar/al 等字段，在反序列化的时候会报 ValidationError，
-                # 所以这里如果检测到 ValidationError，就跳过这首歌曲。
-                #
-                # 可能的修复方法：
-                # 1. 在 SongModel 上加一个 flag 来标识该歌曲是否为云盘歌曲，
-                #    如果是的话，则使用 cloud_song_detail 接口来获取相关信息。
-                # name = song_data['name']
-                # logger.warn(f'cloud song:{name} may not exist on netease, skip it.')
+            if song_data['id'] == song_data['s_id']:
                 extra_songs_info.append((idx, song_data['id']))
-                # song_data = func_extra(str(song_data['id']))[data_field][0]
-                # song = _deserialize(song_data, schema_extra)
             else:
+                song = _deserialize(song_data, schema)
                 songs.append(song)
+            # try:
+            #     song = _deserialize(song_data, schema)
+            #     songs.append(song)
+            # except ValidationError:
+            #     # FIXME: 有些云盘歌曲在 netease 上不存在，这时不能把它们转换成
+            #     # SongModel。因为 SongModel 的逻辑没有考虑 song 不存在的情况。
+            #     # 这类歌曲往往没有 ar/al 等字段，在反序列化的时候会报 ValidationError，
+            #     # 所以这里如果检测到 ValidationError，就跳过这首歌曲。
+            #     #
+            #     # 可能的修复方法：
+            #     # 1. 在 SongModel 上加一个 flag 来标识该歌曲是否为云盘歌曲，
+            #     #    如果是的话，则使用 cloud_song_detail 接口来获取相关信息。
+            #     # name = song_data['name']
+            #     # logger.warn(f'cloud song:{name} may not exist on netease, skip it.')
+            #     extra_songs_info.append((idx, song_data['id']))
+            #     # song_data = func_extra(str(song_data['id']))[data_field][0]
+            #     # song = _deserialize(song_data, schema_extra)
+            # else:
+            #     songs.append(song)
 
         if extra_songs_info:
             extra_song_ids = ','.join([str(id) for (_, id) in extra_songs_info])
