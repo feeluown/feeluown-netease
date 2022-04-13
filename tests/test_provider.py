7481d74a5c36b1b0faed8a73390a68a52edc6c62
cosven <cosven@users.noreply.github.com>
2022-04-14 00:21:45 +0800

    fix: playlist can't load more than 50 songs (#32)

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -4,8 +4,19 @@ from fuo_netease import provider
 
 
 def test_playlist_get(playlist_detail_v3__2829883282):
-    mock_result = mock.patch.object(provider.api, 'playlist_detail_v3',
-                                    return_value=playlist_detail_v3__2829883282)
+    mock.patch.object(provider.api, 'playlist_detail_v3',
+                      return_value=playlist_detail_v3__2829883282)
     identifier = '2829883282'
     model = provider.playlist_get(identifier)
     assert model.identifier == identifier
+
+
+def test_playlist_create_songs_rd(songs_detail_v3__191232):
+    total_count = 8050
+    with mock.patch.object(provider.api, 'playlist_detail_v3',
+                           return_value={'trackIds': [{'id': 1}]*total_count}):
+        with mock.patch.object(provider.api, 'songs_detail_v3',
+                               return_value=songs_detail_v3__191232) as mock_song_detail:
+            provider.playlist_create_songs_rd(mock.Mock()).readall()
+            # 这个调用次数和实现强相关。
+            assert mock_song_detail.call_count == 11  # 8050 = per(50)*1 + per(800)*10
