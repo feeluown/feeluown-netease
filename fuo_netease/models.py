fb909057805e298e26670b3df7c6d6a6777a97a0
cyliuu <lcy940903@gmail.com>
2023-08-04 13:07:43 +0800

    1. 修复云盘上传逻辑 2. 更精准地判断云盘歌曲是否存在于网易云音乐库中

--- fuo_netease/models.py
+++ fuo_netease/models.py
@@ -34,7 +34,7 @@ def create_g(func, schema=None, data_field=None):
     return reader
 
 
-def create_cloud_songs_g(func, func_extra, schema=None, schema_extra=None,
+def create_cloud_songs_g(func, func_private, schema=None, schema_private=None,
                          data_field=None, data_key=None):
     data = func(limit=0)
     if data is None:
@@ -48,35 +48,26 @@ def create_cloud_songs_g(func, func_extra, schema=None, schema_extra=None,
         songs_data = func(start, end - start)
 
         songs = []
-        extra_songs_info = []
+        private_songs_info = []
         for idx, song_data in enumerate(songs_data[data_field]):
             if data_key:
                 song_data = song_data[data_key]
-            try:
-                song = _deserialize(song_data, schema)
-            except ValidationError:
+            if song_data['id'] == song_data['s_id']:
                 # FIXME: 有些云盘歌曲在 netease 上不存在，这时不能把它们转换成
                 # SongModel。因为 SongModel 的逻辑没有考虑 song 不存在的情况。
-                # 这类歌曲往往没有 ar/al 等字段，在反序列化的时候会报 ValidationError，
-                # 所以这里如果检测到 ValidationError，就跳过这首歌曲。
-                #
-                # 可能的修复方法：
-                # 1. 在 SongModel 上加一个 flag 来标识该歌曲是否为云盘歌曲，
-                #    如果是的话，则使用 cloud_song_detail 接口来获取相关信息。
-                # name = song_data['name']
-                # logger.warn(f'cloud song:{name} may not exist on netease, skip it.')
-                extra_songs_info.append((idx, song_data['id']))
-                # song_data = func_extra(str(song_data['id']))[data_field][0]
-                # song = _deserialize(song_data, schema_extra)
+                name = song_data['name']
+                logger.warning(f'cloud song:{name} may not exist on netease.')
+                private_songs_info.append((idx, song_data['id']))
             else:
+                song = _deserialize(song_data, schema)
                 songs.append(song)
 
-        if extra_songs_info:
-            extra_song_ids = ','.join([str(id) for (_, id) in extra_songs_info])
-            extra_songs_data = func_extra(extra_song_ids)
-            for idx, song_data in enumerate(extra_songs_data[data_field]):
-                song = _deserialize(song_data, schema_extra)
-                songs.insert(extra_songs_info[idx][0], song)
+        if private_songs_info:
+            private_song_ids = ','.join([str(id) for (_, id) in private_songs_info])
+            private_songs_data = func_private(private_song_ids)
+            for idx, song_data in enumerate(private_songs_data[data_field]):
+                song = _deserialize(song_data, schema_private)
+                songs.insert(private_songs_info[idx][0], song)
 
         return songs
 
