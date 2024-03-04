35712a87cfa0fc7a974acdf0e6148850298eb531
cosven <yinshaowen241@gmail.com>
2024-03-04 21:21:27 +0800

    bump version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fuo_netease',
-    version='1.0',
+    version='1.0.1',
     description='feeluown netease plugin',
     author='Cosven',
     author_email='yinshaowen241@gmail.com',
